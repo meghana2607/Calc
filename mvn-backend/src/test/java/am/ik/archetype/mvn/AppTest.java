f67779d32a9b9596eac2005d272e63476bc55536
meghana2607 <meghanagunda0@gmail.com>
2021-12-11 16:18:39 +0530

    file

---
+++ mvn-backend/src/test/java/am/ik/archetype/mvn/AppTest.java
@@ -0,0 +1,12 @@
+package am.ik.archetype.mvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
